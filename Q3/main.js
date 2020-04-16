c1d2c6a77980645683bab6e86152a3d8602867a5
Fiby <flamefiby@icloud.com>
2020-04-17 05:48:17 +0800

    Add files via upload

--- Q3/main.js
+++ Q3/main.js
@@ -1,62 +1,42 @@
 // JavaScript Document
 
 // set the json url
-let url = 'https://flamefiby.github.io/COMP1073FinalExam/Q3/cats.json';
-// select button
-let button = document.getElementById('button');
-button.addEventListener('click', fetchAll);
+let url = 'https://flamefiby.github.io/Module-Four/package.json';
+// select section
+let section = document.querySelector('section');
 
-function fetchAll() {
-    fetch(url)
-        .then(function(response){
-        return response.json();
-    }).then(function(json) {
-        cats = json;
-    }).then(function() {
-        displayCats(cats);
-    }).catch(function(err) {
-        console.log('Fetch Error Message: ' + err.message);
-    });
-}
+// using fetch to get the json file in the network
+fetch(url).then(function(response){
+    // return the json data
+    return response.json();
+    }).then(function(products) {
+        // using a for loop to attend each product in the json file
+        for (let i = 0; i < products.strangeProducts.length; i++) {
 
-function displayCats(jsonObj){
-    let cats = jsonObj.cats;
+            //build HTML elements
+            let article = document.createElement('article');
+            let h2 = document.createElement('h2');
+            let img = document.createElement('img');
+            let p1 = document.createElement('p1');
+            let p2 = document.createElement('p2');
+            let ul = document.createElement('ul');
+            // set the img source and name
+            img.setAttribute('src',
+                'https://flamefiby.github.io/Module-Four/img/' + products.strangeProducts[i].image);
+            img.setAttribute('alt', products.strangeProducts[i].image);
+            // given the name, price, and details to each product
+            h2.textContent = products.strangeProducts[i].name;
+            p1.textContent = 'Price, ' + products.strangeProducts[i].price;
+            p2.textContent = 'details, ' + products.strangeProducts[i].details;
+            // append each product
+            article.appendChild(img);
+            article.appendChild(h2);
+            article.appendChild(p1);
+            article.appendChild(p2);
+            section.appendChild(article);
 
-    for (let i = 0; i < cats.length; i++) {
-        //build HTML elements
-        let section = document.querySelector('section');
-        let article = document.createElement('article');
-        let h2 = document.createElement('h2');
-        let p1 = document.createElement('p1');
-        let p2 = document.createElement('p2');
-        let p3 = document.createElement('p3');
-        let img = document.createElement('img');
-        // set the img source and name
-        img.setAttribute('src',
-            'https://flamefiby.github.io/COMP1073FinalExam/Q3/assets/' + cats[i].photo);
-        img.setAttribute('alt', cats[i].photo);
-
-        // given the name, price, and details to each product
-        h2.textContent = cats[i].name;
-        p1.textContent = 'Species: ' + cats[i].species;
-        p2.textContent = 'Favorite Food: ' + cats[i].favFoods;
-        p3.textContent = 'Age:' + cats[i].age;
-        // append each product
-        article.appendChild(img);
-        article.appendChild(h2);
-        article.appendChild(p1);
-        article.appendChild(p2);
-        article.appendChild(p3);
-        section.appendChild(article);
-
-    }
-}
-
-
-
-
-
-
+        }
+});
 
 
 
